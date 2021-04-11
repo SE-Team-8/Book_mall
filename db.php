e2dfc98c4ece037cce71619b5a524d38c135bae8
ParkHwanYong <77776372+ParkHwanYong@users.noreply.github.com>
2021-04-11 19:52:35 +0900

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
  <?php
   session_start();
   
-  $db = new mysqli("localhost","root","xampp","signup");
+  $db = new mysqli("127.0.0.1","root","FLvguSbVkG6N","SignUp");
   $db->set_charset("utf8");
 
   function mq($sql){
