5819074ae8f21edfe52ce2ef405061f919376484
ParkHwanYong <77776372+ParkHwanYong@users.noreply.github.com>
2021-04-11 17:46:37 +0900

    Update register_user.php

--- register_user.php
+++ register_user.php
@@ -1,6 +1,6 @@
 <?php
 header("Content-Type: text/html;charset=UTF-8");
-$conn = mysqli_connect("127.0.0.1","root","FLvguSbVkG6N","signup");
+$conn = mysqli_connect("127.0.0.1","root","FLvguSbVkG6N","SignUp");
 $data_stream = "'".$_GET['Id']."','".$_GET['Password']."','".$_GET['Name']."','".$_GET['PhoneNum']."','".$_GET['Email']."'";
 $query = "insert into user(`Id`, `Password`, `Name`, `PhoneNum`, `Email`) values (".$data_stream.")";
 $result = mysqli_query($conn, $query);
